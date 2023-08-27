397fc8b17a4df335957563451a76ad38895ecff0
MC STYLE <143294260+mc-style@users.noreply.github.com>
2023-08-26 20:12:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ var chatButton = document.createElement("div");
 chatButton.className = "chat-button";
 document.body.appendChild(chatButton);
 
-// Función para abrir el bot
-function abrirBot() {
-  // Aquí podrías agregar la lógica para mostrar el bot o abrir una ventana de chat
+// Función para detectar si el dispositivo es móvil
+function esDispositivoMovil() {
+  return window.innerWidth <= 768; // Puedes ajustar este valor según tus necesidades
+}
+
+// Función para abrir el bot en dispositivos móviles
+function abrirBotMovil() {
   alert("¡Hola! ¿En qué puedo ayudarte?");
 }
 
-// Agregar un evento de clic al botón flotante
-chatButton.addEventListener("click", abrirBot);
+// Función para abrir otra ventana de chat en dispositivos de escritorio
+function abrirOtraVentana() {
+  // Implementa aquí tu lógica para abrir una ventana de chat en dispositivos de escritorio
+  alert("¡Hola! Estamos aquí para ayudarte.");
+}
+
+// Event listener para el botón flotante
+chatButton.addEventListener("click", function() {
+  if (esDispositivoMovil()) {
+    abrirBotMovil(); // Abre el bot en dispositivos móviles
+  } else {
+    abrirOtraVentana(); // Abre otra ventana de chat en dispositivos de escritorio
+  }
+});
