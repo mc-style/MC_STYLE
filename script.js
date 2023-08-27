c71ad946f273b96bf3ab86db033cbc8f9f4d5219
MC STYLE <143294260+mc-style@users.noreply.github.com>
2023-08-26 20:04:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Puedes agregar interacciones y funcionalidades aquí utilizando JavaScript
+// Crear el botón flotante
+var chatButton = document.createElement("div");
+chatButton.className = "chat-button";
+document.body.appendChild(chatButton);
+
+// Función para abrir el bot
+function abrirBot() {
+  // Aquí podrías agregar la lógica para mostrar el bot o abrir una ventana de chat
+  alert("¡Hola! ¿En qué puedo ayudarte?");
+}
+
+// Agregar un evento de clic al botón flotante
+chatButton.addEventListener("click", abrirBot);
